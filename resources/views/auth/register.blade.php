0732c5a0b98e7078a863dbb5d79af6d7d95ad1ad
hencaladarodriguez <hencaladarodriguez@gmail.com>
2026-02-22 15:45:50 +0100

    Ajustes del Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,12 +13,19 @@
         <h2>Registro</h2>
         <form id="registerForm">
 
+            <p>Nombre</p>
             <input type="text" name="nombre" required>
+            <p>Apellido</p>
             <input type="text" name="apellidos">
+            <p>Email</p>
             <input type="email" name="email" required>
+            <p>Contraseña</p>
             <input type="password" name="password" required>
+            <p>Fecha de Nacimiento</p>
             <input type="date" name="fecha_nacimiento" required>
+            <p>Peso Base</p>
             <input type="number" step="0.1" name="peso_base" required>
+            <p>Altura Base</p>
             <input type="number" name="altura_base" required>
 
             <button type="submit">Crear cuenta</button>
